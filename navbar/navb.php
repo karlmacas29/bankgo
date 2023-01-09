df3524468c8d674767c3ba0e907fd36e10d60e3c
karlmacas29 <karlracomamacas@gmail.com>
2023-01-09 16:38:19 +0800

    modified:   navbar/navb.php

--- navbar/navb.php
+++ navbar/navb.php
@@ -1,9 +1,9 @@
 
 <div class="sidebar bg-dark" >
   <a href="dashboard.php" class="text-white mb-1 mx-1" id="active1" title="Balance"><i class="bi bi-wallet2"></i> <span class="c5">  Balance</span></a>
-  <a href="../bankgo/withdraw.php" class="text-white mb-1 mx-1" id="active2" title="Withdraw"><i class="bi bi-cash-stack"></i> <span class="c5">  Withdraw Money</span> </a>
-  <a href="../bankgo/deposit.php" class="text-white mb-1 mx-1" id="active3" title="Deposit"><i class="bi bi-safe"></i> <span class="c5">  Deposit Money</span> </a>
-  <a href="../bankgo/transaction.php" class="text-white mb-1 mx-1" id="active4" title="Transaction"><i class="bi bi-file-text"></i> <span class="c5">  Transaction </span><span class="span3 badge text-bg-primary "><?php
+  <a href="./withdraw.php" class="text-white mb-1 mx-1" id="active2" title="Withdraw"><i class="bi bi-cash-stack"></i> <span class="c5">  Withdraw Money</span> </a>
+  <a href="./deposit.php" class="text-white mb-1 mx-1" id="active3" title="Deposit"><i class="bi bi-safe"></i> <span class="c5">  Deposit Money</span> </a>
+  <a href="./transaction.php" class="text-white mb-1 mx-1" id="active4" title="Transaction"><i class="bi bi-file-text"></i> <span class="c5">  Transaction </span><span class="span3 badge text-bg-primary "><?php
   if ($cnt == 0) {
 
   } else {
